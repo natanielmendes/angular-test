604a88c38d5367f89a91ca296bbba0f815cbf947
Nataniel Mendes <natanielmendes@hotmail.com>
2025-06-23 11:51:14 -0300

    Using RXJS to fetch user data

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subject, of } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap, filter, startWith } from 'rxjs/operators';
+import { Observable, Subject, combineLatest, of } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, filter, startWith, map, shareReplay } from 'rxjs/operators';
 
 interface User {
   id: number;
@@ -16,46 +16,37 @@ interface User {
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
-  users: User[] = [];
-  filteredUsers: User[] = [];
+  users$: Observable<User[]>;
+  filteredUsers$!: Observable<User[]>;
   searchTerm$ = new Subject<string>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.users$ = this.http.get<User[]>('https://jsonplaceholder.typicode.com/users').pipe(shareReplay(1));
+  }
 
   ngOnInit() {
-    this.http.get<User[]>('https://jsonplaceholder.typicode.com/users').subscribe(users => {
-      this.users = users;
-      this.filteredUsers = users;
-    });
-
-    this.searchTerm$
-      .pipe(
+    this.filteredUsers$ = combineLatest([
+      this.users$,
+      this.searchTerm$.pipe(
         startWith(''),
         debounceTime(2000),
         distinctUntilChanged(),
-        filter(term => term.length === 0 || term.length >= 2),
-        switchMap(term => this.filterUsers(term))
+        filter(term => term.length === 0 || term.length >= 2)
       )
-      .subscribe(filtered => {
-        this.filteredUsers = filtered;
-      });
+    ]).pipe(
+      map(([users, term]) => {
+        if (!term) return users;
+        const lowerTerm = term.toLowerCase();
+        return users.filter(user =>
+          user.name.toLowerCase().includes(lowerTerm) ||
+          user.username.toLowerCase().includes(lowerTerm) ||
+          user.email.toLowerCase().includes(lowerTerm)
+        );
+      })
+    );
   }
 
   onSearch(event: any) {
     this.searchTerm$.next(event.target.value);
-  }
-
-  private filterUsers(term: string) {
-    if (!term) {
-      return of([...this.users]);
-    }
-    const lowerTerm = term.toLowerCase();
-    return of(
-      this.users.filter(user =>
-        user.name.toLowerCase().includes(lowerTerm) ||
-        user.username.toLowerCase().includes(lowerTerm) ||
-        user.email.toLowerCase().includes(lowerTerm)
-      )
-    );
   }
 }
